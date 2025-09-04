cba11d8c7f817e1881bbe71cc647b084ee31cbbd
Dipankar Ghosh <dgkgecece@gmail.com>
2025-09-05 01:17:28 +0530

    Episode-3

--- App.js
+++ App.js
@@ -14,23 +14,45 @@
     </div>
 </div>
 */
+// import React from "react";
+// import ReactDOM from 'react-dom/client';
+
+// const parent = React.createElement('div',
+//     {id: 'parent'},
+//     [
+//         React.createElement('div', {id: 'child1', key: 'child1'},
+//         [
+//             React.createElement('h1', {key: 'h1-1'}, "I'm an h1 tag"),
+//             React.createElement('h2', {key: 'h2-1'}, "I'm an h2 tag")
+//         ]),
+//         React.createElement('div', {id: 'child2', key: 'child2'},
+//         [
+//             React.createElement('h1', {key: 'h1-2'}, "I'm an h1 tag"),
+//             React.createElement('h2', {key: 'h2-2'}, "I'm an h2 tag")
+//         ])
+//     ]);
+// console.log(parent);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(parent);
+
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement('div',
-    {id: 'parent'},
-    [
-        React.createElement('div', {id: 'child1', key: 'child1'},
-        [
-            React.createElement('h1', {key: 'h1-1'}, "I'm an h1 tag"),
-            React.createElement('h2', {key: 'h2-1'}, "I'm an h2 tag")
-        ]),
-        React.createElement('div', {id: 'child2', key: 'child2'},
-        [
-            React.createElement('h1', {key: 'h1-2'}, "I'm an h1 tag"),
-            React.createElement('h2', {key: 'h2-2'}, "I'm an h2 tag")
-        ])
-    ]);
-console.log(parent);
+const heading = <h1>Heading 1</h1>; //React Element
+const Title = () => (
+    <h1>Namaste React By Akshay</h1>
+);
+
+// Functional Component
+const Heading = () => ( //JSX
+    <div id="container">
+        {Title()}
+        <Title />
+        <Title></Title>
+        {heading}
+        <p>This is React Heading Component</p>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+root.render(<Heading />);
