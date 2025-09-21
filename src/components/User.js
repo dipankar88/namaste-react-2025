cb40aba7c74ccc34033bf5648609d22305a333d3
Dipankar Ghosh <dgkgecece@gmail.com>
2025-09-22 01:34:33 +0530

    create state variable and update state variable using setState for class component

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 import '../styles/User.css';
 
 const User = ({name, location}) => {
+    const [count, setCount] = useState(0);
     return (
         <div className="user-card">
+            <div className='count-fn'>
+                <h1>Count: {count}</h1>
+                <button className='increment' onClick={() => setCount(count + 1)}>+</button>
+            </div>
+
             <h2>Name: {name}</h2>
             <h3>Location: {location}</h3>
             <h3>Learning: JavaScript and React</h3>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,12 +4,26 @@ class UserClass extends React.Component {
     constructor(props){
         super(props);
         // this.name = props.name;
+        this.state = {
+            count: 0,
+            count2: 1
+        }
     }
 
     render(){
         const {name, location} = this.props;
+        const {count, count2} = this.state;
         return (
             <div className="user-card">
+                <div className='count-fn'>
+                    <h1>Count: {count}</h1>
+                    <button
+                    className='increment'
+                    onClick={() => this.setState({
+                        count: this.state.count + 1
+                    })}>+</button>
+                </div>
+                <h1>Class-Count: {count2}</h1>
                 <h2>Name: {name}</h2>
                 <h3>Location: {location}</h3>
                 <h3>Learning: JavaScript and React</h3>
